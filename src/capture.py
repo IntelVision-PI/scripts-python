4116dc741aea56da3f4f3f67d720aa77f8898a6e
Nicolas Barbosa <nicolas.pereira009@sptech.school>
2025-11-23 09:23:53 -0300

    arrumando a função para enviar o arquivo quando virar o dia e limpar o csv local

--- src/capture.py
+++ src/capture.py
@@ -8,15 +8,49 @@ import datetime as dt
 import time
 
 csv_file = "dados_maquina.csv"
-contador = 0
+user = "servidor6.NICOLASF8SK4U2"
 
-while contador <= 4:
-    user = "servidor6.NICOLASF8SK4U2"
+def upload_file(file_name, bucket):
+    agora = dt.datetime.now()
+    ano = agora.strftime("%Y")
+    mes = agora.strftime("%m")
+    dia = agora.strftime("%d")
+
+    nome_s3 = f"dados_maquina_{ano}-{mes}-{dia}--{user}.csv"
+    object_name = f"{ano}/{mes}/{dia}/{nome_s3}"
+
+    s3_client = boto3.client('s3')
+
+    try:
+        s3_client.upload_file(file_name, bucket, object_name)
+        print(f"Arquivo enviado para o S3 em: {object_name}")
+    except ClientError as e:
+        logging.error(e)
+        return False
+
+    return True
+
+
+ultimo_dia = dt.datetime.now().day 
+
+while True:
+
+    agora = dt.datetime.now()
+    if agora.day != ultimo_dia:
+        print("⏰ Deu meia-noite! Enviando arquivo e limpando CSV...")
+
+        upload_file(csv_file, "my-bucket-raw-sptech-nicolas")
+
+        open(csv_file, "w").close()
+        print("CSV limpo.\n")
+
+        ultimo_dia = agora.day 
+
 
     processos_objs = []
     for proc in ps.process_iter(['pid', 'name']):
         try:
-            proc.cpu_percent(interval=None)  
+            proc.cpu_percent(interval=None)
             processos_objs.append(proc)
         except (ps.NoSuchProcess, ps.AccessDenied, ps.ZombieProcess):
             pass
@@ -29,11 +63,11 @@ while contador <= 4:
     disk_usage = ps.disk_usage('/').percent
     disk_size = round(ps.disk_usage('/').total / 1024**3, 2)
     bytes_recv_usage = ps.net_io_counters().bytes_recv
-    packages_recv_usage = ps.net_io_counters().bytes_recv
+    packages_recv_usage = ps.net_io_counters().packets_recv
     bytes_sent_usage = ps.net_io_counters().bytes_sent
     packages_sent_usage = ps.net_io_counters().packets_sent
     
-    timestamp = dt.datetime.now().replace(microsecond=0)
+    timestamp = agora.replace(microsecond=0)
     active_processes = len(processos_objs)
 
     processos_info = []
@@ -44,11 +78,12 @@ while contador <= 4:
                 'name': proc.name(),
                 'cpu': round(cpu_p, 2),
             })
-        except (ps.NoSuchProcess, ps.AccessDenied, ps.ZombieProcess):
+        except:
             pass
 
     processos_ordenados = sorted(processos_info, key=lambda p: p['cpu'], reverse=True)
     top5 = processos_ordenados[:5]
+
     while len(top5) < 5:
         top5.append({'name': None, 'cpu': 0.0})
 
@@ -73,32 +108,9 @@ while contador <= 4:
 
     df = pd.DataFrame(dados)
 
-    if not os.path.isfile(csv_file):
+    if not os.path.isfile(csv_file) or os.path.getsize(csv_file) == 0:
         df.to_csv(csv_file, mode='a', encoding='utf-8', sep=';', index=False)
     else:
         df.to_csv(csv_file, mode='a', encoding='utf-8', sep=';', header=False, index=False)
-    
+
     time.sleep(10)
-    contador += 1
-
-def upload_file(file_name, bucket):
-    agora = dt.datetime.now()
-    ano = agora.strftime("%Y")
-    mes = agora.strftime("%m")
-    dia = agora.strftime("%d")
-
-    nome_s3 = f"dados_maquina_{ano}-{mes}-{dia}.csv"
-    object_name = f"{ano}/{mes}/{dia}/{nome_s3}"
-
-    s3_client = boto3.client('s3')
-
-    try:
-        s3_client.upload_file(file_name, bucket, object_name)
-        print(f"Arquivo enviado para o S3 em: {object_name}")
-    except ClientError as e:
-        logging.error(e)
-        return False
-
-    return True
-
-upload_file(csv_file, 'my-bucket-raw-sptech')
